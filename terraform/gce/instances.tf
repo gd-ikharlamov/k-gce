8c4202db86b4915f489175f0a18f59bbf51bca62
Ilya Kharlamov <ikharlamov@griddynamics.com>
2017-11-03 17:35:46 +0300

    add terraform provision for sonarqube

--- terraform/gce/instances.tf
+++ terraform/gce/instances.tf
@@ -1,8 +1,8 @@
 resource "google_compute_instance" "mysql" {
-  name             = "mysql-${terraform.workspace}"
-  machine_type     = "${var.machine_type}"
-  zone             = "${var.region}"
-  tags             = ["mysql"]
+  name         = "mysql-${terraform.workspace}"
+  machine_type = "${var.machine_type}"
+  zone         = "${var.region}"
+  tags         = ["mysql"]
 
   boot_disk {
     initialize_params {
@@ -21,11 +21,11 @@ resource "google_compute_instance" "mysql" {
 }
 
 resource "google_compute_instance" "sonarqube" {
-  depends_on      = ["google_compute_instance.mysql"]
-  name            = "sonarqube-${terraform.workspace}"
-  machine_type    = "${var.machine_type}"
-  zone            = "${var.region}"
-  tags            = ["sonarqube"]
+  depends_on   = ["google_compute_instance.mysql"]
+  name         = "sonarqube-${terraform.workspace}"
+  machine_type = "${var.machine_type}"
+  zone         = "${var.region}"
+  tags         = ["sonarqube"]
 
   boot_disk {
     initialize_params {
@@ -40,5 +40,19 @@ resource "google_compute_instance" "sonarqube" {
 
   metadata {
     "ssh-keys" = "centos:${file("~/.ssh/ikharlamov-nb.pub")}"
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "centos"
+      private_key = "${file("~/.ssh/ikharlamov-nb")}"
+      agent       = "false"
+    }
+
+    inline = [
+      "sudo sed 's/mysql:3306/${google_compute_instance.mysql.network_interface.0.address}:3306/g' -i /opt/sonarsource/sonarqube-5.6.7/conf/sonar.properties",
+      "sudo systemctl reboot sonarqube.service"
+    ]
   }
 }
