9b6e3294eeddbe459d3700913f2fe491cf89ea01
Ilya Kharlamov <ikharlamov@griddynamics.com>
2017-11-10 16:19:50 +0300

    update terraform output

--- terraform/gce/outputs.tf
+++ terraform/gce/outputs.tf
@@ -2,10 +2,34 @@ output "environment" {
   value = "${terraform.workspace}"
 }
 
-output "mysql internal ip" {
-  value = "${google_compute_instance.mysql.network_interface.0.address}"
+output "mysql internal ips" {
+  value = "${join(", ", google_compute_instance.mysql.*.network_interface.0.address)}"
+}
+
+output "mysql external ips" {
+  value = "${join(", ", google_compute_instance.mysql.*.network_interface.0.access_config.0.assigned_nat_ip)}"
 }
 
 output "sonarqube internal ips" {
-  value = "${join(",", google_compute_instance.sonarqube.*.network_interface.0.address)}"
+  value = "${join(", ", google_compute_instance.sonarqube.*.network_interface.0.address)}"
+}
+
+output "sonarqube external ips" {
+  value = "${join(", ", google_compute_instance.sonarqube.*.network_interface.0.access_config.0.assigned_nat_ip)}"
+}
+
+output "consul external ips" {
+  value = "${join(", ", google_compute_instance.consul.*.network_interface.0.access_config.0.assigned_nat_ip)}"
+}
+
+output "consul internal ips" {
+  value = "${join(", ", google_compute_instance.consul.*.network_interface.0.address)}"
+}
+
+output "vault external ips" {
+  value = "${join(", ", google_compute_instance.vault.*.network_interface.0.access_config.0.assigned_nat_ip)}"
+}
+
+output "vault internal ips" {
+  value = "${join(", ", google_compute_instance.vault.*.network_interface.0.address)}"
 }
